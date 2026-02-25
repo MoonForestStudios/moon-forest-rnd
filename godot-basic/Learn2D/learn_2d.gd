ef68ff8e15c42d91a8dc7367dc28c35a20ee37aa
F3liP3L <felipe7u7432@gmail.com>
2026-02-25 00:52:01 -0500

    ✨ feat: add say function and variables with diferent scope

--- godot-basic/Learn2D/learn_2d.gd
+++ godot-basic/Learn2D/learn_2d.gd
@@ -6,17 +6,28 @@ var numero_real = 12.4
 var otro_numero_real = 3.1416
 
 var edad_juan = 25
-var edad_felipe = 23
+const edad_felipe = 23
+
+var suma_edades = 0
 
 ''' Función que corre el proyecto'''
 func _ready():
 	print('Entrando')
-	var suma_edades = edad_felipe + edad_juan # 48
+	suma_edades = edad_felipe + edad_juan # 48
 	var resta_edades = edad_felipe - edad_juan # -2
 	var multiplicar_edades = edad_felipe * edad_juan
 	var division_edades = edad_felipe / edad_juan
 	var modulo_edades = edad_felipe % edad_juan
 	
-	print(suma_edades)
+	print("Suma edades " + str(suma_edades))
+	print("Resta edades " + str(resta_edades))
+	print("Multiplicación edades " + str(multiplicar_edades))
+	print("División edades " + str(division_edades))
+	print("Módulo edades " + str(modulo_edades))
+
+	hola()
 	pass
+
+func hola():
+	print("La suma es: " + str(suma_edades))
 	
