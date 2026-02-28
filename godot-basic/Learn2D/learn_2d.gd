6556b10abe3d5f71fa3d6be441cde83f117f1627
F3liP3L <felipe7u7432@gmail.com>
2026-02-28 00:22:00 -0500

    ✨ feat: comment out _ready function to avoid junk logs

--- godot-basic/Learn2D/learn_2d.gd
+++ godot-basic/Learn2D/learn_2d.gd
@@ -11,24 +11,24 @@ const edad_felipe = 23
 var suma_edades = 0
 
 ''' Función que corre el proyecto'''
-func _ready():
-	print('\n\t Nodo Padre')
-	suma_edades = edad_felipe + edad_juan # 48
-	var resta_edades = edad_felipe - edad_juan # -2
-	var multiplicar_edades = edad_felipe * edad_juan
-	var division_edades = edad_felipe / edad_juan
-	var modulo_edades = edad_felipe % edad_juan
-	
-	print("Suma edades " + str(suma_edades))
-	print("Resta edades " + str(resta_edades))
-	print("Multiplicación edades " + str(multiplicar_edades))
-	print("División edades " + str(division_edades))
-	print("Módulo edades " + str(modulo_edades))
-
-	hola()
-	
-	print("Final nodo padre...")
-	pass
+#func _ready():
+	#print('\n\t Nodo Padre')
+	#suma_edades = edad_felipe + edad_juan # 48
+	#var resta_edades = edad_felipe - edad_juan # -2
+	#var multiplicar_edades = edad_felipe * edad_juan
+	#var division_edades = edad_felipe / edad_juan
+	#var modulo_edades = edad_felipe % edad_juan
+	#
+	#print("Suma edades " + str(suma_edades))
+	#print("Resta edades " + str(resta_edades))
+	#print("Multiplicación edades " + str(multiplicar_edades))
+	#print("División edades " + str(division_edades))
+	#print("Módulo edades " + str(modulo_edades))
+#
+	#hola()
+	#
+	#print("Final nodo padre...")
+	#pass
 
 func hola():
 	print("La suma es: " + str(suma_edades))
--- godot-basic/Learn2D/learn_string.gd
+++ godot-basic/Learn2D/learn_string.gd
@@ -2,25 +2,25 @@ extends Node2D
 
 var text: String = "Hola, Godot 4."
 
-func _ready():
-	print(text+"\n")
-	
-	var name = "Pipe"
-	#var say = "Hola, "+name
-	var say = "Hola, %s" % name
-	var sentence = "El nombre de mi amigo es %s y mi hermano %s" % ["Juan", "Diego"]
-	
-	var length = sentence.length()
-	var value = "Godot"
-	var other = "  gOdot  "
-	var otherSay = "Duplicate say"
-	var findText = "say"
-	
-	print(sentence, "\nLa longitud es: %d" % length)
-	print("Texto is empty: %s" % text.is_empty())
-	print(value.to_upper())
-	print(value.to_lower())
-	print(other.strip_edges())
-	print(otherSay.replace("say", "the say"))
-	print(otherSay.find(findText))
-	print("La palabra esta dentro del texto: %s" % otherSay.contains(findText))
+#func _ready():
+	#print(text+"\n")
+	#
+	#var name = "Pipe"
+	##var say = "Hola, "+name
+	#var say = "Hola, %s" % name
+	#var sentence = "El nombre de mi amigo es %s y mi hermano %s" % ["Juan", "Diego"]
+	#
+	#var length = sentence.length()
+	#var value = "Godot"
+	#var other = "  gOdot  "
+	#var otherSay = "Duplicate say"
+	#var findText = "say"
+	#
+	#print(sentence, "\nLa longitud es: %d" % length)
+	#print("Texto is empty: %s" % text.is_empty())
+	#print(value.to_upper())
+	#print(value.to_lower())
+	#print(other.strip_edges())
+	#print(otherSay.replace("say", "the say"))
+	#print(otherSay.find(findText))
+	#print("La palabra esta dentro del texto: %s" % otherSay.contains(findText))
