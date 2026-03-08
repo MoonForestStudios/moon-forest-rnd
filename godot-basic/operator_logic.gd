f6737b78b23547d8b457dda3e8799a410dea91ba
F3liP3L <felipe7u7432@gmail.com>
2026-03-07 22:40:49 -0500

    ✨ feat: add average and athlete variables to operator logic script

--- godot-basic/operator_logic.gd
+++ godot-basic/operator_logic.gd
@@ -2,16 +2,23 @@ extends Node2D
 
 @export var num1: int
 @export var num2: int
+@export var average: float
+@export var athlete: String
 
 func _ready() -> void:
 	if num1 > 0 && num2 > 0:
 		print("Ambos numeros son mayores que 0.")
-	else:
-		print("Los numeros no son mayores que 0.")
-	numberValidation()
+	numberValidationWithAnd()
+	scholarshipAverageWithOr()
 
-func numberValidation() -> void:
+func numberValidationWithAnd() -> void:
 	if (num1 > 0 and num2 <= 10) and (num2 > 0 and num2 <= 10):
 		print("Ambos numeros son mayor a 0 y menor a 10.")
 	else:
 		print("Ambos numeros no son mayor a 0 y menor a 10.")
+		
+func scholarshipAverageWithOr() -> void:
+	if (average >= 18 || athlete == "Si"):
+		print("Puedes acceder a una beca!!")
+	else:
+		print("No cumples los requisitos.")
