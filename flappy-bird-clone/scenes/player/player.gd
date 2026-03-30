c1d488cd2019454ac9fb21708b0958981bb4a4a1
F3liP3L <felipe7u7432@gmail.com>
2026-03-29 23:59:48 -0500

    ♻️ refactor: clean up comments and remove unnecessary whitespace in player.gd

--- flappy-bird-clone/scenes/player/player.gd
+++ flappy-bird-clone/scenes/player/player.gd
@@ -6,7 +6,7 @@ extends CharacterBody2D
 @export var rotation_speed := 2.0
 
 func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
+	velocity.y += gravity * delta # delta permite respetar los FPS de forma coherente entre dispositivos.
 	''' Es lo mismo que usar la función min, la idea es regular la velocidad.
 	if (velocity.y >= max_speed):
 		velocity.y = max_speed 
@@ -15,7 +15,6 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_pressed("jump"):
 		jump()
-	
 	
 	move_and_slide() # Hace que se ejecute el movimiento del Nodo o Player
 	rotatePlayer()
@@ -27,4 +26,3 @@ func jump() -> void:
 func rotatePlayer() -> void:
 	if (velocity.y  > 0 and rad_to_deg(rotation) < 90):
 		rotation += rotation_speed * deg_to_rad(1)
-		
