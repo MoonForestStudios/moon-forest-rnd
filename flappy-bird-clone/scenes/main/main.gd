e8e36e6e6765d8aca0127f7f38a45b0075b8cb95
F3liP3L <felipe7u7432@gmail.com>
2026-05-20 01:04:59 -0500

    ✨ feat: implement spawner functionality to manage obstacle spawning and integrate with main scene

--- flappy-bird-clone/scenes/main/main.gd
+++ flappy-bird-clone/scenes/main/main.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
+@onready var spawner: Spawner = $Spawner
+
 func _on_player_on_game_started() -> void:
-	print("El signal ha sido exitoso!")
+	spawner.timer.start()
 
 func _on_obstacle_on_player_crashed() -> void:
 	print("Colision!")
--- flappy-bird-clone/scenes/obstacles/obstacle.gd
+++ flappy-bird-clone/scenes/obstacles/obstacle.gd
@@ -9,3 +9,6 @@ func _process(delta: float) -> void:
 
 func _on_area_body_entered(body: Node2D) -> void:
 	on_player_crashed.emit()
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
